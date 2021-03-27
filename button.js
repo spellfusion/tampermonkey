1364bafe451bca5fc2b0ccdc3925b91e28a90c0c
Andreas Scherren <abdalamah@gmail.com>
2021-03-27 18:58:59 +0100

    Adding append button

--- button.js
+++ button.js
@@ -24,3 +24,17 @@ Button.Floating = (label, onClick) => {
     Button.Margins.right += label.length * Button.MarginPerTextLength + Button.MarginPerButton;
     document.body.appendChild(button);
 };
+
+Button.Append = (id, label, onClick) => {
+    let button = document.createElement("button");
+    button.innerText = label;
+    button.onclick = onClick;
+    button.style = "background: linear-gradient(#51c2ff, #006da0);"+
+        "border: none;"+
+        "border-radius: 5px;"+
+        "padding: 5px 10px;"+
+        "margin: 5px 10px;"+
+        "cursor: pointer;"+
+        "color: white;";
+    $(id).append(button);
+};
